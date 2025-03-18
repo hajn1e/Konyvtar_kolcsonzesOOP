9b0565555402874e01bc73b5024d48625daae9f4
hajn1e <thexhaj888@gmail.com>
2025-03-18 10:07:27 +0100

    full

---
+++ Könyvtar_kolcsonzesOOP/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Könyvtar_kolcsonzesOOP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+            Konyvtar konyvtar = new Konyvtar();
+            konyvtar.Konyvek = Fajlkezelo.KonyvekBetoltese();
+        
+            Felhasznalo felhasznalo = new Felhasznalo("Teszt Elek");
+        
+            Konyv ujKonyv = new Konyv("C# alapok", "Koczka", "1234567890");
+            konyvtar.HozzaadKonyv(ujKonyv);
+        
+            konyvtar.ListazKonyveket();
+        
+            Konyv konyv = konyvtar.KeresKonyv("1234567890");
+            if (konyv != null)
+            {
+                felhasznalo.Kolcsonoz(konyv);
+            }
+            Fajlkezelo.KonyvekMentese(konyvtar.Konyvek);
+        
+            konyvtar.ListazKonyveket();
+        
+    }
+}
